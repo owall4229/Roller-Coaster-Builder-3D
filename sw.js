5a9fbc88eadd4380e5653878d859671683712de1
PandaBefriender <84032271+owall4229@users.noreply.github.com>
2025-05-26 10:25:56 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `core-gaming-app`;
+const CACHE_NAME = `Roller Coaster Builder 3D`;
     
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
